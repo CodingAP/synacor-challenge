fc2fc15ab65533a3bd233ecfed11c0cd37aba647
CodingAP <apsoutherngaming07@gmail.com>
2020-12-18 21:45:45 -0600

    starting it again

--- synacor.js
+++ synacor.js
@@ -1,6 +1,18 @@
+const fs = require('fs');
+
 class SynacorChallenge {
     constructor() {
-        this.memory = new Array(0xffff);
+        this.memory = new Array(0x7fff);
+
+        this.registers = new Array(8);
+
+        this.instructions = [
+            {
+                callback: args => {
+                    
+                }
+            }
+        ]
     }
 
     run() {
